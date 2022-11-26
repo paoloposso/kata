91424161010ef13d9e1ac0e3b47c3793ab7bf703
paolo <pvictorsys@gmail.com>
2022-11-26 12:56:18 -0300

    insert at

--- go/frontend-masters-course/doubly_linked_list/main.go
+++ go/frontend-masters-course/doubly_linked_list/main.go
@@ -1,7 +1,13 @@
 package main
 
+import (
+	"errors"
+)
+
 type LinkedList struct {
-	head *Node
+	head   *Node
+	tail   *Node
+	length int
 }
 
 type Node struct {
@@ -11,32 +17,77 @@ type Node struct {
 }
 
 func (l *LinkedList) append(value string) {
+	l.length++
 	newNode := &Node{value: value}
 	if l.head == nil {
 		l.head = newNode
+		l.tail = newNode
 		return
 	}
-	tail := l.head
-	for tail.next != nil {
-		tail = tail.next
-	}
+	tail := l.tail
 	tail.next = newNode
 	tail.next.previous = tail
+	l.tail = newNode
 }
 
 func (l *LinkedList) prepend(value string) {
+	l.length++
 	newNode := &Node{value: value}
-	if l.head != nil {
-		newNode.next = l.head
-		l.head.previous = newNode
+	if l.head == nil {
+		l.head = newNode
+		l.tail = newNode
+		return
 	}
+	newNode.next = l.head
+	l.head.previous = newNode
 	l.head = newNode
+}
+
+func (l *LinkedList) printList() {
+	cur := l.head
+	for cur != nil {
+		print(cur.value + "->")
+		cur = cur.next
+	}
+}
+
+func (l *LinkedList) insertAt(value string, index int) error {
+	if l.length < index {
+		return errors.New("index must be equals or minor list length")
+	}
+	l.length++
+	if index == 0 {
+		l.prepend(value)
+		return nil
+	}
+	if index == l.length-1 {
+		l.append(value)
+		return nil
+	}
+	newNode := &Node{value: value}
+	nex := l.head
+	for i := 0; i < index; i++ {
+		nex = nex.next
+	}
+	newNode.next = nex
+	nex.previous.next = newNode
+	newNode.previous = nex.previous
+	nex.previous = newNode
+	return nil
 }
 
 func main() {
 	ll := LinkedList{}
+	ll.prepend("11111")
 	ll.append("aaaa")
 	ll.append("bbb")
 	ll.append("bbbssssdsd")
 	ll.prepend("ffff")
+	ll.prepend("ffxxx")
+	ll.append("ffxxx")
+	ll.insertAt("insertedAt 3", 8)
+
+	ll.printList()
+	println("")
+	println(ll.length)
 }

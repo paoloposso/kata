b53f9c05a44970ba15f8ff5c3e641f49871b441b
Paolo Posso <pvictorsys@gmail.com>
2022-12-28 18:42:48 -0300

    Generate random maze and print it out

--- go/frontend-masters-course/recursivity/maze/main.go
+++ go/frontend-masters-course/recursivity/maze/main.go
@@ -1,10 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 //todo - finish
 
 type Point struct {
 	x int
 	y int
+}
+
+func generateRandomMaze(x int, y int, sparseness int) (maze [][]string, start Point, end Point) {
+	for i := 0; i < x; i++ {
+		line := []string{}
+
+		for j := 0; j < y; j++ {
+			line = append(line, " ")
+		}
+		maze = append(maze, line)
+	}
+
+	end = getRandomPoint(x, y)
+	maze[end.x][end.y] = "$"
+
+	start = getRandomPoint(x, y)
+	maze[start.x][start.y] = "@"
+
+	return maze, start, end
+}
+
+func printMaze(maze [][]string) {
+	for i := 0; i < len(maze); i++ {
+		for j := 0; j < len(maze[i]); j++ {
+			fmt.Printf("[%s]", maze[i][j])
+		}
+		fmt.Println()
+	}
 }
 
 func Walk(maze [][]string, wall string, current Point, end Point, seen [][]bool, pathStack []Point) bool {
@@ -32,5 +66,19 @@ func Walk(maze [][]string, wall string, current Point, end Point, seen [][]bool,
 }
 
 func main() {
+	maze, _, _ := generateRandomMaze(4, 4, 0)
+	printMaze(maze)
+}
 
+func getRandomPoint(maxX, maxY int) Point {
+
+	result := Point{}
+
+	rand.Seed(time.Now().UnixNano())
+	result.x = rand.Intn(maxX)
+
+	rand.Seed(time.Now().UnixNano())
+	result.y = rand.Intn(maxY)
+
+	return result
 }
